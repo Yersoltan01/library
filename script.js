e11a0d563e5f84c81468bb450c20303486f2fdea
Yersoltan <natlosrey@gmail.com>
2022-06-20 09:28:51 +0600

    Wrap add-books in form

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
+// remove or add book's 'read' class by clicking on it's status 
 document.querySelector('#books').addEventListener('click', function(e) {
   if (e.target && e.target.matches('.book-status')) {
     document.querySelector(`#${e.target.id}`).classList.toggle('read');
   }
 })
 
+let myLibrary = [];
+
+function Book(name, author, alreadyRead) {
+  this.name = name
+  this.author = author
+  this.alreadyRead = alreadyRead
+}
+
+function addBookToLibrary() {
+  
+}
